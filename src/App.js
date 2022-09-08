59b57c8d25d36ccfcf8393278a4fd46299bf4681
Sharon Mwenda <smkathambi@gmail.com>
2022-09-08 14:15:38 +0300

    Added image, added main section in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import React from 'react';
 import './App.css';
+import logo from "public/logo.jpeg";
 
 function App() {
   return (
     <div>
-      <h2>Restaurant review App</h2>
+      <main>
+        <section className='menu section'>
+          <div className='title'>
+            <img src={logo} alt="logo" className='logo'/>
+            <h2>Restaurant review List</h2>
+            <div className='underline'></div>
+          </div>
+        </section>
+      </main>
+      
     </div>
   );
 }
